11e0a72b043ff43a052d6087278df40675740f39
zhudong <zhudongdong@91guoxin.com>
2017-08-28 16:51:40 +0800

    1111

---
+++ framework/version.inc.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * [WeEngine System] Copyright (c) 2014 WE7.CC
+ * WeEngine is NOT a free software, it under the license terms, visited http://www.we7.win/for more details.
+ */
+defined('IN_IA') or exit('Access Denied');
+
+define('IMS_FAMILY', 'x');
+define('IMS_VERSION', '1.4.6');
+define('IMS_RELEASE_DATE', '201707070001');
